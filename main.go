cbaae76d66020e6a70dedc93a59a2f624fa33c99
dog0sd <ryhorkanoplich@gmail.com>
2025-06-26 01:07:19 +0200

    as server + config

--- main.go
+++ main.go
@@ -1,83 +1,118 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 	"time"
+	"encoding/json"
 
 	"github.com/haguro/elevenlabs-go"
-
 	"github.com/hajimehoshi/go-mp3"
 	"github.com/hajimehoshi/oto"
-	"github.com/joho/godotenv"
+	"github.com/jinzhu/configor"
 )
 
-const (
-	movel    = "eleven_monolinguak_v1"
-	voice_id = "pNInz6obpgDQGcFmaJgB"
+type Config struct {
+	Elevenlabs struct {
+		Enabled bool `default:"false"`
+		VoiceId string `required:"true"`
+		Model string `default:"eleven_multilingual_v2"`
+		Token string `required:"true"`
+	}
+	Port string `default:"8080"`
+}
+
+type TTSRequest struct {
+	Text string
+}
+
+var (
+	config Config
 )
 
 func main() {
-	err := godotenv.Load()
+	err := configor.Load(&config, "sven.yml")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+	elevenlabsToken := config.Elevenlabs.Token
+	voiceId := config.Elevenlabs.VoiceId
+	model := config.Elevenlabs.Model
 
-	elevenlabs_token := os.Getenv("ELEVEN_LABS_API_KEY")
 	var text string
 	if len(os.Args) == 1 {
-		scanner := bufio.NewScanner(os.Stdin)
-		for scanner.Scan() {
-			text += scanner.Text() + " "
-		}
+		fmt.Println("Running as server on " + config.Port + "...")
+		http.HandleFunc("/tts", handleTTS)
+		log.Fatal(http.ListenAndServe(":" + config.Port, nil))
 	} else {
 		text = strings.Join(os.Args[1:], " ")
 	}
-	client := elevenlabs.NewClient(context.Background(), elevenlabs_token, 30*time.Second)
+	err = elevenlabsTTS(text, model, voiceId, elevenlabsToken)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v", err)
+		os.Exit(1)
+	}
+}
+
+func handleTTS(w http.ResponseWriter, r *http.Request) {
+	req := TTSRequest{}
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error decoding request")
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	err = elevenlabsTTS(req.Text, config.Elevenlabs.Model, config.Elevenlabs.VoiceId, config.Elevenlabs.Token)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "elevelabs error: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte("OK"))
+}
+
+func elevenlabsTTS(text string, model string, voiceId string, token string) error {
+
+	client := elevenlabs.NewClient(context.Background(), token, 30*time.Second)
 	ttsReq := elevenlabs.TextToSpeechRequest{
 		Text:    text,
-		ModelID: "eleven_monolingual_v1",
+		ModelID: model,
 	}
-	audio, err := client.TextToSpeech(voice_id, ttsReq)
+	audio, err := client.TextToSpeech(voiceId, ttsReq)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	// Write the audio file bytes to disk
-	if err := os.WriteFile("adam.mp3", audio, 0644); err != nil {
-		log.Fatal(err)
+	if err := os.WriteFile("audio.mp3", audio, 0644); err != nil {
+		return err
 	}
 
-	// test
-	f, err := os.Open("adam.mp3")
+	f, err := os.Open("audio.mp3")
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(1)
+		return err
 	}
 	defer f.Close()
 
 	d, err := mp3.NewDecoder(f)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(1)
+		return err
 	}
 
 	c, err := oto.NewContext(d.SampleRate(), 2, 2, 8192)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(1)
+		return err
 	}
 	defer c.Close()
 
 	p := c.NewPlayer()
 	defer p.Close()
 	if _, err := io.Copy(p, d); err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(1)
+		return err
 	}
+	return nil
 }
--- scripts/script.sh
+++ scripts/script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+set -e
+
 
 GOARCH=amd64 GOOS=windows go build -o build/app.exe . && powershell.exe -Command ".\\build\\app.exe $@"
-rm build/app.exe
-rm adam.mp3
+rm -f build/app.exe
+rm -f audio.mp3
